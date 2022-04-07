654e8daa425b1ec2bfcb5fbb218865d8be040d2d
Artem110055 <101782337+Artem110055@users.noreply.github.com>
2022-04-07 11:34:24 +0600

    Add files via upload

---
+++ лаба 3, 1.py
@@ -0,0 +1,6 @@
+print ('Введите А:')
+А=input()
+print('Введите В:')
+B=input()
+if A==B:
+    print('A равно В')
---
+++ лаба 3, 2.py
@@ -0,0 +1,8 @@
+print('Введите А:')
+A=input()
+print('Введите В:')
+B=input()
+if A==B:
+    print('А равно В')
+else:
+    print('A не равно В')
